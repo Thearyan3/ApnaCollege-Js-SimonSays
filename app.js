4138d074bc6359cc51f5dd2e24fc237598dc6b2a
Aryan <chaudharyaryan3456@gmail.com>
2025-01-30 19:57:52 +0530

    Created CSS

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 let gameSeq = [];
 let userSeq = [];
 
+
+
 let start = false;
 let level = 0;
 
-document.addEventListener("keypress", function(){
-    if(start == false){
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if (start == false) {
         console.log("Game is started!");
         start = true;
+
+        levelUp();
     }
-})
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+
+let btns = ['yellow', 'red', 'green', 'purple'];
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+
+    let randIndx = Math.floor(Math.random() * 3);
+    let randCol = btns[randIndx];
+    let randBtn = document.querySelector(`.${randCol}`);
+    btnFlash(randBtn);
+}
